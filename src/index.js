6bb63c025037b7068295378e0a3ed0d79b4b6222
Ian DeJong <ianrdejong@gmail.com>
2022-04-25 08:25:50 -0700

    Render React component on top of p5 canvas

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('p5-canvas-container'));
 root.render(
   <React.StrictMode>
     <App />
